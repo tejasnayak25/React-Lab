299f6f42f92237759f5171ea5b2443953df2e026
Tejas Nayak <arpithanayak900@gmail.com>
2026-02-03 11:55:32 +0530

    Add files via upload

---
+++ program2/Footer.jsx
@@ -0,0 +1,15 @@
+import PropTypes from "prop-types";
+
+const Footer = ({ copyright }) => {
+    return (
+        <footer>
+            <p>{copyright}</p>
+        </footer>
+    );
+};
+
+Footer.propTypes = {
+    copyright: PropTypes.string.isRequired
+}
+
+export default Footer;
