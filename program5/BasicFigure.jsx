c99713d4af5eaea6292e3ed5d071cef53ba9fd32
Tejas Nayak <arpithanayak900@gmail.com>
2026-02-17 12:32:06 +0530

    Add files via upload

---
+++ program5/BasicFigure.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line react/prop-types
+const BasicFigure = ({ src, caption, onRemove }) => {
+    return (
+        <div className="image-card">
+            <img src={src} alt="Gallery" />
+            <div className="caption">{caption}</div>
+            <button onClick={onRemove}>Remove</button>
+        </div>
+    );
+};
+
+export default BasicFigure;
